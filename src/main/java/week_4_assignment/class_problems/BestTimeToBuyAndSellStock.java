dbacb8fcd8fd8764697cf8639e969f568b81738b
ps9102-hub <ps9102@srmist.edu.in>
2026-09-12 00:55:41 +0530

    Add all class problems for session 4

---
+++ src/main/java/week_4_assignment/class_problems/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,28 @@
+public class BestTimeToBuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                int currentProfit = prices[i] - minPrice;
+                if (currentProfit > maxProfit) {
+                    maxProfit = currentProfit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices1 = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices1)); // Output: 5
+
+        int[] prices2 = {7, 6, 4, 3, 1};
+        System.out.println(maxProfit(prices2)); // Output: 0
+    }
+}
